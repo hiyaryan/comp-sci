4e4c24510df6082de27bbb4a1b5bddaacb3f8a6a
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-26 14:02:12 -0800

    Resistor color
    How to define and use TypeScript types, including object index signatures and function return types, and how to handle required function parameters.

---
+++ langs/exercism/typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,33 @@
+export const colors: { [key: string]: number } = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const colorCode = (name: string): number => {
+  if (name === undefined) {
+    return -1;
+  }
+
+  return colors[name];
+};
+
+export const COLORS: string[] = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
