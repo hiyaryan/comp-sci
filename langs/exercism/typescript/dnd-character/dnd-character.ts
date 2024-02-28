64e8299d595f559dd4b9b4792fad24b78e98ecca
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-28 13:56:18 -0800

    D&D character
    How to use classes, properties, constructors, static methods, array methods like sort and reduce, and the Math object for generating random numbers and rounding down.

---
+++ langs/exercism/typescript/dnd-character/dnd-character.ts
@@ -0,0 +1,35 @@
+export class DnDCharacter {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  hitpoints: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+  }
+
+  public static generateAbilityScore(): number {
+    let diceRolls = [
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1,
+    ];
+    diceRolls.sort((a, b) => b - a);
+    diceRolls.pop();
+    return diceRolls.reduce((a, b) => a + b, 0);
+  }
+
+  public static getModifierFor(abilityValue: number): number {
+    return Math.floor((abilityValue - 10) / 2);
+  }
+}
