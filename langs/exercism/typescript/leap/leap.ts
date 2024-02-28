43bca832111e1f42deebc5d131807a3a31002f5c
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-27 19:45:01 -0800

    Leap
    How to use function types, conditional (if) statements, modulus operator for remainder calculation, and boolean return values.

---
+++ langs/exercism/typescript/leap/leap.ts
@@ -0,0 +1,12 @@
+export function isLeap(year: number): boolean {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
